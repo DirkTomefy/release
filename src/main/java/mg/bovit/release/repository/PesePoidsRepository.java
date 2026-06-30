86594ece33fd224fc917aaf096a688170c892181
CHARLES <amelsoanasycharles@gmail.com>
2026-06-30 14:07:46 +0300

    create repository for pese_poids

---
+++ src/main/java/mg/bovit/release/repository/PesePoidsRepository.java
@@ -0,0 +1,12 @@
+package mg.bovit.release.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+import mg.bovit.release.model.*;
+
+@Repository
+public interface PesePoidsRepository extends JpaRepository<PesePoids, Long> {
+
+}
