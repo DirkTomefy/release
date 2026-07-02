2350165e06bcf7f923219d52fe58ea3b25c69d5a
mahatoky <andrian.mahatoky@gmail.com>
2026-07-02 09:52:01 +0300

    init base

--- database/data.sql
+++ database/data.sql
@@ -91,3 +91,31 @@ CREATE TABLE mvt_caisse(
         REFERENCES caisse(id)
 
 );
+
+CREATE TABLE client (
+    id SERIAL PRIMARY KEY,
+    nom VARCHAR(100) NOT NULL,
+    prenom VARCHAR(100) NOT NULL,
+    contact VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE vente_bovin (
+    id SERIAL PRIMARY KEY,
+    id_client INT NOT NULL,
+    description VARCHAR(200),
+    DATE date_vente DATE NOT NULL DEFAULT CURRENT_DATE,
+
+    CONSTRAINT fk_vente_bovin_client FOREIGN KEY (id_client)
+    REFERENCES client(id)
+);
+
+CREATE Table vente_detail (
+    id SERIAL PRIMARY KEY,
+    id_vente INT NOT NULL,
+    id_bovin INT NOT NULL,
+
+    CONSTRAINT fk_vente_detail_vente FOREIGN KEY (id_vente)
+    REFERENCES id_vente(id),
+    CONSTRAINT fk_vente_detail_bovin FOREIGN KEY (id_bovin)
+    REFERENCES id_bovin(id)
+)
