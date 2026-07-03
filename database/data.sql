5d9f16b8b3900c6020fcc1798f1e8f428b0d09e3
Sarubidy <rantsoavinatolotrasarobidy@gmail.com>
2026-07-03 15:01:07 +0300

    fix bug database

--- database/data.sql
+++ database/data.sql
@@ -86,7 +86,7 @@ CREATE TABLE mvt_caisse(
     montant  DOUBLE PRECISION NOT NULL,
     id_caisse INTEGER NOT NULL,
 
-    CONSTRAINT fk_mvt_caisse_caisseFOREIGN KEY (id_caisse)
+    CONSTRAINT fk_mvt_caisse_caisse FOREIGN KEY (id_caisse)
 
         REFERENCES caisse(id)
 
