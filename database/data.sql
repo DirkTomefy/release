74c1cbbfa5a23f6a56c49f999f1928e16421f368
mahatoky <andrian.mahatoky@gmail.com>
2026-07-03 07:38:48 +0300

    [fact] : changement de place du script sql vente

--- database/data.sql
+++ database/data.sql
@@ -1,4 +1,4 @@
--- Active: 1781599410280@@127.0.0.1@5432@bovin_db
+-- Active: 1779074118545@@127.0.0.1@5432@bovin_db
 CREATE DATABASE bovin_db;
 \c bovin_db;
 
@@ -91,31 +91,3 @@ CREATE TABLE mvt_caisse(
         REFERENCES caisse(id)
 
 );
-
-CREATE TABLE client (
-    id SERIAL PRIMARY KEY,
-    nom VARCHAR(100) NOT NULL,
-    prenom VARCHAR(100) NOT NULL,
-    contact VARCHAR(100) NOT NULL
-);
-
-CREATE TABLE vente_bovin (
-    id SERIAL PRIMARY KEY,
-    id_client INT NOT NULL,
-    description VARCHAR(200),
-    date_vente DATE NOT NULL DEFAULT CURRENT_DATE,
-
-    CONSTRAINT fk_vente_bovin_client FOREIGN KEY (id_client)
-    REFERENCES client(id)
-);
-
-CREATE Table vente_detail (
-    id SERIAL PRIMARY KEY,
-    id_vente INT NOT NULL,
-    id_bovin INT NOT NULL,
-
-    CONSTRAINT fk_vente_detail_vente FOREIGN KEY (id_vente)
-    REFERENCES vente_bovin(id),
-    CONSTRAINT fk_vente_detail_bovin FOREIGN KEY (id_bovin)
-    REFERENCES bovin(id)
-)
