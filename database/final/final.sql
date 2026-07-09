443b0069a5c85a4985213d7bb643c11a89cb285c
DirkTomefy <DirkTomefy@gmail.com>
2026-07-09 08:26:54 +0300

    [fix] : fixé final.sql

--- database/final/final.sql
+++ database/final/final.sql
@@ -22,9 +22,7 @@ DROP TABLE IF EXISTS caisse CASCADE;
 
 DROP TABLE IF EXISTS type_materiel CASCADE;
 DROP TABLE IF EXISTS materiel CASCADE;
-DROP TABLE IF EXISTS mvt_stock_entree CASCADE;
-DROP TABLE IF EXISTS mvt_stock_entree_paiement CASCADE;
-DROP TABLE IF EXISTS mvt_stock_sortie CASCADE;
+DROP TABLE IF EXISTS mvt_stock CASCADE;
 DROP TABLE IF EXISTS inventaire_detail CASCADE;
 DROP TABLE IF EXISTS inventaire CASCADE;
 
@@ -167,45 +165,22 @@ INSERT INTO materiel (libelle, id_type_materiel, type_gestion) VALUES
     ('Ustensile B', 2, 'LIFO'),
     ('Autre A', 3, 'FIFO');
 
--- Nouvelle table : mvt_stock_entree
-CREATE TABLE mvt_stock_entree (
-    id SERIAL PRIMARY KEY,
-    id_materiel INTEGER NOT NULL,
-    prix_unitaire DOUBLE PRECISION NOT NULL,
-    qte DOUBLE PRECISION NOT NULL,
-    qte_restant DOUBLE PRECISION NOT NULL,
-    date_entree DATE DEFAULT CURRENT_DATE,
-    
-    CONSTRAINT fk_mvt_stock_materiel
-        FOREIGN KEY (id_materiel)
-        REFERENCES materiel(id)
-);
 
-CREATE TABLE mvt_stock_entree_paiement (
+CREATE TABLE mvt_stock_paiement (
     id SERIAL PRIMARY KEY,
-    id_mvt_stock_entree INTEGER NOT NULL,
+    id_mvt_stock INTEGER NOT NULL,
     id_caisse INTEGER NOT NULL,
     montant DOUBLE PRECISION NOT NULL,
 
-    CONSTRAINT fk_mvt_stock_entree_paiement -- payement ou paiement ? -> paiement
-        FOREIGN KEY (id_mvt_stock_entree)
-        REFERENCES mvt_stock_entree(id),
+    CONSTRAINT fk_mvt_stock_paiement -- payement ou paiement ? -> paiement
+        FOREIGN KEY (id_mvt_stock)
+        REFERENCES mvt_stock(id),
     
     CONSTRAINT fk_mvt_stock_caisse
         FOREIGN KEY (id_caisse)
         REFERENCES caisse(id)
 );
 
-CREATE TABLE mvt_stock_sortie (
-    id SERIAL PRIMARY KEY,
-    id_materiel INTEGER NOT NULL,
-    qte DOUBLE PRECISION NOT NULL,
-    date_sortie DATE DEFAULT CURRENT_DATE,
-
-    CONSTRAINT fk_mvt_stock_sortie_materiel
-        FOREIGN KEY (id_materiel)
-        REFERENCES materiel(id)
-);
 
 
 -- ============================================================
