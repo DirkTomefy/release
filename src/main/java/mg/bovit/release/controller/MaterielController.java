6fea08b5f725a6f28599a17dfb01f5b520748456
mahatoky <andrian.mahatoky@gmail.com>
2026-07-11 04:42:51 +0300

    controller pour afficher l etat de stock du materiel ,et gere le filtre multicritaire

--- src/main/java/mg/bovit/release/controller/MaterielController.java
+++ src/main/java/mg/bovit/release/controller/MaterielController.java
@@ -10,14 +10,35 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import mg.bovit.release.dto.MaterielStockDto;
+import mg.bovit.release.dto.MultiCriteriaEtatStockMateriel;
 import mg.bovit.release.model.MouvementStock;
 import mg.bovit.release.service.MaterielService;
+import mg.bovit.release.service.MaterielTypeService;
+import mg.bovit.release.service.MouvementStockService;
 
 @Controller
 @RequestMapping("/materiel")
 public class MaterielController {
+
+    @Autowired
+    private MaterielTypeService materielTypeService;
+
     @Autowired
     private MaterielService materielService;
+
+    @Autowired
+    private MouvementStockService mouvementStockService;
+
+    @GetMapping("/etatstock")
+    public String showEtatStockView(MultiCriteriaEtatStockMateriel criteria ,Model model) {
+        model.addAttribute("typeMateriels",materielTypeService.findAll());
+        model.addAttribute("materiels",materielService.findAll());
+        model.addAttribute("criteria",criteria);
+
+        model.addAttribute("stock", mouvementStockService.searchEtatStock(criteria));
+
+        return "materiel/etatstock";
+    }
 
     @GetMapping("/liste")
     public String getMaterielListe(Model model) {
