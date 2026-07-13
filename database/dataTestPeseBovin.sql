1bb2c62a5a7c357d1cd45b6861b16e45fb557faf
DirkTomefy <DirkTomefy@gmail.com>
2026-07-13 20:25:13 +0300

    [fix] : supprimer tous les sql non utiles

--- database/dataTestPeseBovin.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO public.pese_bovin (id_bovin,date_pese,poids_apres) VALUES (36,'2026-07-04',120);
-
-INSERT INTO public.pese_bovin (id_bovin,date_pese,poids_apres) VALUES (36,'2026-07-05',130);
-
-
-INSERT INTO public.pese_bovin (id_bovin,date_pese,poids_apres) VALUES (36,'2026-07-06',200);
